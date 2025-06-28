ce17671e817f22b4fe767844c1ab9cf109204fe2
metisted19 <metisted19@gmail.com>
2025-06-28 18:46:57 +0200

    Fix port to 8080 for Railway

--- server.py
+++ server.py
@@ -54,5 +54,4 @@ def paypal_webhook():
     return jsonify({"status": "ignored"}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  # 🔁 utilise le port donné par Railway
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
